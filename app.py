02222bf18e1e4ed54661a830ec079a3db5f197bf
Thomas Scrivner <Thomas.M.Scrivner-1@ou.edu>
2023-09-12 17:10:45 -0500

    Multiplication Function (Problem 2)

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-def addNums():
-    numbers = [1,2,3,4,5,6,7,8,9.10]
+numbers = [1,2,3,4,5,6,7,8,9.10]
+
+def addNums(numbers):
+    
     sum = 0
     for i in numbers:
         sum += numbers[i]
 
     return sum
 
+def multiplyNums(numbers):
+    multiTotal = 0
+    for i in numbers: 
+        multiTotal *= numbers[i]
 
+    return multiTotal
